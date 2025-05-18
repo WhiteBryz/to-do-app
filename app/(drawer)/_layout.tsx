0070541a9acd1bc3a669e40349c91dc4d270cd4f
Mazon64 <dayaramon64@gmail.com>
2025-05-18 15:23:18 -0600

    Enhance theming support in DrawerLayout and HistoryScreen by applying theme properties to styles and components

--- app/(drawer)/_layout.tsx
+++ app/(drawer)/_layout.tsx
@@ -8,33 +8,50 @@ import '../../utils/debug';
 
 
 export default function DrawerLayout() {
-    const theme = useTheme(); 
+  const theme = useTheme();
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <Drawer drawerContent={(props) => <CustomDrawer {...props} />}
+      <Drawer
+        drawerContent={(props) => <CustomDrawer {...props} />}
         screenOptions={{
-    drawerActiveTintColor: theme.primary,
-    drawerInactiveTintColor: theme.text,
-    drawerStyle: { backgroundColor: theme.background },
-  }}>
-        <Drawer.Screen name="home" options={{ title: 'Tareas', drawerIcon: ({color, size, focused}) => {
-          return <Ionicons name="pencil-outline" color={focused ? theme.primary : theme.text} size={size} />;
-        }}} />
-        <Drawer.Screen name="trophies/index" options={{ title: 'Trofeos', drawerIcon: ({color, size, focused}) =>{
-          return <Ionicons name="trophy-outline" color={focused ? theme.primary : theme.text} size={size} />;
-        } }} />
-        <Drawer.Screen name="charts/index" options={{ title: 'Gráficas', drawerIcon: ({color, size, focused}) =>{
-          return <Ionicons name="pie-chart-outline" color={focused ? theme.primary : theme.text} size={size} />;
-        }
+          drawerActiveTintColor: theme.primary,
+          drawerInactiveTintColor: theme.text,
+          drawerStyle: { backgroundColor: theme.background },
+
+          headerStyle: {
+            backgroundColor: theme.background,
+          },
+          headerTitleStyle: {
+            color: theme.text,
+            fontWeight: 'bold',
+          },
+          headerTintColor: theme.text,
+        }}
+      >
+        <Drawer.Screen name="home" options={{
+          title: 'Tareas', drawerIcon: ({ color, size, focused }) => {
+            return <Ionicons name="pencil-outline" color={focused ? theme.primary : theme.text} size={size} />;
+          }
+        }} />
+        <Drawer.Screen name="trophies/index" options={{
+          title: 'Trofeos', drawerIcon: ({ color, size, focused }) => {
+            return <Ionicons name="trophy-outline" color={focused ? theme.primary : theme.text} size={size} />;
+          }
+        }} />
+        <Drawer.Screen name="charts/index" options={{
+          title: 'Gráficas', drawerIcon: ({ color, size, focused }) => {
+            return <Ionicons name="pie-chart-outline" color={focused ? theme.primary : theme.text} size={size} />;
+          }
         }} />
         <Drawer.Screen name="history/index" options={{
           title: 'Historial', drawerIcon: ({ color, size, focused }) => {
             return <Ionicons name="time-outline" color={focused ? theme.primary : theme.text} size={size} />;
           }
         }} />
-        <Drawer.Screen name="settings/index" options={{ title: 'Configuración', drawerIcon: ({color, size, focused}) =>{
-          return <Ionicons name="settings-outline" color={focused ? theme.primary : theme.text} size={size} />;
-        }
+        <Drawer.Screen name="settings/index" options={{
+          title: 'Configuración', drawerIcon: ({ color, size, focused }) => {
+            return <Ionicons name="settings-outline" color={focused ? theme.primary : theme.text} size={size} />;
+          }
         }} />
       </Drawer>
     </GestureHandlerRootView>
