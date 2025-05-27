e04262b55f59387aecdf88623c88cc4ed13f01ee
WhiteBryz <davidflores@live.com.mx>
2025-05-26 22:49:52 -0600

    Add new functionalities in logout from authService

--- services/authService.ts
+++ services/authService.ts
@@ -1,4 +1,6 @@
 // authService.ts
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { cancelAllScheduledNotificationsAsync } from "expo-notifications";
 import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../utils/firebaseConfig";
 
@@ -11,6 +13,13 @@ export const register = async (email: string, password: string) => {
 };
 
 export const logout = async () => {
+  // Borramos credenciales y datos del almacenamiento local
+  await AsyncStorage.clear();
+
+  // Cancelamos todas las notificaciones de la aplicación
+  await cancelAllScheduledNotificationsAsync();
+
+  // Cerramos sesión en firebase
   return await auth.signOut();
 };
 
